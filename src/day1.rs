4872f8f08a54c6365a66d71799188860d12e4dab
devis350 <erik.turzinski@gmail.com>
2020-12-02 01:31:23 +0100

    use Result/Option as return type use find_map() instead of find().map()

--- src/day1.rs
+++ src/day1.rs
@@ -1,22 +1,23 @@
+use std::num::ParseIntError;
+
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    input.lines().map(|s| s.parse().unwrap()).collect()
+pub fn input_generator(input: &str) -> Result<Vec<u32>, ParseIntError> {
+    input.lines().into_iter().map(str::parse).collect()
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &[u32]) -> u32 {
+pub fn solve_part1(input: &[u32]) -> Option<u32> {
     input.iter()
         .enumerate()
         .find_map(|(i, &num)| {
             input.iter()
                 .skip(i)
-                .find(|&num1| num + num1 == 2020)
-                .map(|&num1| num * num1)
-        }).unwrap()
+                .find_map(|&num1| if num + num1 == 2020 { Some(num * num1) } else { None })
+        })
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &[u32]) -> u32 {
+pub fn solve_part2(input: &[u32]) -> Option<u32> {
     input.iter()
         .enumerate()
         .find_map(|(i, &num)| {
@@ -25,10 +26,8 @@ pub fn solve_part2(input: &[u32]) -> u32 {
                 .enumerate()
                 .find_map(|(j, &num1)| {
                     input.iter()
-                        .skip(i+j)
-                        .find(|&num2| num + num1 + num2 == 2020)
-                        .map(|&num2| num * num1 * num2)
+                        .skip(i + j)
+                        .find_map(|&num2| if num + num1 + num2 == 2020 { Some(num * num1 * num2) } else { None })
                 })
         })
-        .unwrap()
 }
